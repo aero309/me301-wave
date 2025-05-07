644046e20bc0c704baec50e250b2f21114d71016
aero309 <khanhky@outlook.com>
2025-05-07 14:24:45 +0200

    log

--- Load_cell/Load_cell.ino
+++ Load_cell/Load_cell.ino
@@ -2,8 +2,8 @@
 #include "HX711.h"
 
 // HX711 circuit wiring
-const int LOADCELL_DOUT_PIN = 2;
-const int LOADCELL_SCK_PIN = 3;
+const int LOADCELL_DOUT_PIN = 9;
+const int LOADCELL_SCK_PIN = 10;
 int MeasureTime;
 HX711 scale;
 
